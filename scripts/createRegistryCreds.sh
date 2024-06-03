ca48494cb2a4d4cd0a203e42a7639aa4554e93b9
ifazia <iguetf@gmail.com>
2024-06-03 09:40:35 +0200

    modif code source services

--- scripts/createRegistryCreds.sh
+++ scripts/createRegistryCreds.sh
@@ -1,3 +1,4 @@
 # This will create a regcreds file to apply to the spring-petclinic namespace 
 
-kubectl create secret docker-registry regcred -n spring-petclinic --docker-server=<REGISTRY> --docker-username=<USERNAME> --docker-password=<PASSWORD> --docker-email=<EMAIL_ADDRESS> --dry-run=client -o yaml > ./k8s/init-namespace/02-regcreds.yaml
+#kubectl create secret docker-registry regcred -n spring-petclinic --docker-server=<REGISTRY> --docker-username=<USERNAME> --docker-password=<PASSWORD> --docker-email=<EMAIL_ADDRESS> --dry-run=client -o yaml > ./k8s/init-namespace/02-regcreds.yaml
+kubectl create secret docker-registry regcred -n production --docker-server=<REGISTRY> --docker-username=<USERNAME> --docker-password=<PASSWORD> --docker-email=<EMAIL_ADDRESS> --dry-run=client -o yaml > ./k8s/init-namespace/02-regcreds.yaml
