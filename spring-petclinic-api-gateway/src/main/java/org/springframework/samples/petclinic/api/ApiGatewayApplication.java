8692f2aa063b694fcaf7010b56dde6f42a7b9aa3
ifazia <iguetf@gmail.com>
2024-07-10 14:11:46 +0200

    deploy app

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayApplication.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayApplication.java
@@ -46,8 +46,7 @@ import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args); 
-       
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
     @Bean
