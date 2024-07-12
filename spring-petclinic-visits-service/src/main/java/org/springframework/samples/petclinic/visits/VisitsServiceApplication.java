0d8dbb931b459167f3f5233c1a8a5d4f41363060
ifazia <iguetf@gmail.com>
2024-07-12 17:23:49 +0200

    build visits service

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
@@ -18,7 +18,7 @@ package org.springframework.samples.petclinic.visits;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//just for tests
+
 /**
  * @author Maciej Szarlinski
  */
