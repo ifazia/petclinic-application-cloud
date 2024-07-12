f71b4a1dfac2a36cb5f4d448f286efd8688dca23
ifazia <iguetf@gmail.com>
2024-07-12 17:57:40 +0200

    build api-gateway

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayDiscoveryConfig.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayDiscoveryConfig.java
@@ -3,7 +3,7 @@ package org.springframework.samples.petclinic.api;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
+//just for tests
 //@Profile("!cloud")
 @Configuration
 @EnableDiscoveryClient
