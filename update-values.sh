4d69129461b768f23fe5063b886818674c94454f
ifazia <iguetf@gmail.com>
2024-07-15 16:25:14 +0200

    test recup dernière version

--- update-values.sh
+++ update-values.sh
@@ -3,8 +3,15 @@
 # Fonction pour mettre à jour le tag de version dans staging-values.yaml
 update_service_version() {
   local service_name="$1"
+  local image_tag="$2"
   local values_file="helmchart/staging-values.yaml"
-  
+
+  echo "Updating $service_name in $values_file to tag $image_tag"
+
+  # Utilisation de sed pour remplacer le tag de version dans le fichier YAML
+  sed -i "s|^\(\s*${service_name}:\s*\n\s*image:\s*public.ecr.aws/i7s8l3z4/${service_name}\s*\n\s*version:\s*\).*|\1${image_tag}|" "$values_file"
+}
+
 # Liste des microservices à mettre à jour
 services=(
   "spring-petclinic-api-gateway"
@@ -13,25 +20,16 @@ services=(
   "spring-petclinic-visits-service"
 )
 
-# Parcourir chaque microservice et mettre à jour le tag de version dans staging-values.yaml
+# Parcourir chaque microservice pour récupérer le tag d'image et mettre à jour le tag de version dans staging-values.yaml
 for service in "${services[@]}"; do
-    # Récupérer le tag d'image pour le service spécifié
-  local image_tag=$(aws ecr-public describe-images --repository-name "$service_name" --query "reverse(sort_by(imageDetails, &imagePushedAt))[0].imageTags[0]" --output text)
+  # Récupérer le tag d'image pour le service spécifié
+  image_tag=$(aws ecr-public describe-images --repository-name "$service" --query "reverse(sort_by(imageDetails, &imagePushedAt))[0].imageTags[0]" --output text)
   if [ -z "$image_tag" ]; then
-    echo "Error: Failed to retrieve image tag for $service_name from ECR Public."
+    echo "Error: Failed to retrieve image tag for $service from ECR Public."
     exit 1
   fi
 
-  echo "Updating $service_name in $values_file to tag $image_tag"
-
-  # Utilisation de sed pour remplacer le tag de version dans le fichier YAML
-  sed -i "s|^\(\s*${service_name}:\s*\n\s*image:\s*public.ecr.aws/i7s8l3z4/${service_name}\s*\n\s*version:\s*\).*|\1${image_tag}|" "$values_file"
-}
-
-  update_service_version "$service"
+  update_service_version "$service" "$image_tag"
 done
 
-
-
- 
 echo "Values updated successfully."
