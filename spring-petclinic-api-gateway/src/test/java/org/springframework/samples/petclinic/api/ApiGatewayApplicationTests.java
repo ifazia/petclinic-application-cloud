acac9d16d370209617e502a62822d1bdce0df6cb
ifazia <iguetf@gmail.com>
2024-10-28 14:25:54 +0100

    deploy app

--- spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/ApiGatewayApplicationTests.java
+++ spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/ApiGatewayApplicationTests.java
@@ -3,13 +3,13 @@ package org.springframework.samples.petclinic.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
+import org.springframework.test.context.TestPropertySource;
 @ActiveProfiles("test")
 @SpringBootTest
 @TestPropertySource(locations = {
-    "classpath:application-primary.yaml",
-    "classpath:application-secondary.yaml",
-    "classpath:application-tertiary.yaml"
+    "classpath:spring-petclinic-customers-service/src/test/resources/application-primary.yaml"
+    "classpath:spring-petclinic-vets-service/src/test/resources/application-secondary.yaml"
+    "classpath:spring-petclinic-visits-service/src/test/resources/application-tertiary.yaml"
 })
 class ApiGatewayApplicationTests {
 
