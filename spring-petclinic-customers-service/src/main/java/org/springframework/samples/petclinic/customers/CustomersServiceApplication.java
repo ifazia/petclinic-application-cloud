63627d883003b86c45676ca569aa9e195e550b7c
ifazia <iguetf@gmail.com>
2024-10-29 17:46:23 +0100

    deploy customers service

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -29,5 +29,5 @@ public class CustomersServiceApplication {
 		SpringApplication.run(CustomersServiceApplication.class, args);
 	}
 
+
 }
-//just for test
--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/security/InsecurityConfiguration.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/security/InsecurityConfiguration.java
@@ -2,34 +2,30 @@ package org.springframework.samples.petclinic.customers.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
-public class InsecurityConfiguration {
+public class InsecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final static Logger log = LoggerFactory.getLogger(InsecurityConfiguration.class);
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         log.warn("configuring insecure HttpSecurity");
-        http
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            )
-            .httpBasic().disable()
-            .csrf().disable();
-        return http.build();
+        http.
+        	authorizeRequests().anyRequest().permitAll()
+        	.and()
+        	.httpBasic().disable()
+        	.csrf().disable();
     }
 
-    @Bean
+    @Override
     public void configure(WebSecurity web) throws Exception {
         log.warn("configuring insecure WebSecurity");
-        web.ignoring().requestMatchers("/**"); // Utilisez requestMatchers au lieu de antMatchers
+        web.ignoring().antMatchers("/**");
     }
+
 }
